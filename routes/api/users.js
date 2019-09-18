ec36dc5a7d15f6236786f2d56ac92021fc485a16
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-09-18 14:35:06 -0700

    DB connection added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,39 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../../models/User');
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // @route   POST api/users/register
 // @desc    Register a user
 // @access  Public
-router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      return res.status(400).json({ email: 'Email already exists' })
-    } else {
-      const avatar = gravatar.url(req.body.email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-      });
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      });
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm"
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
 
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) throw err;
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
+        bcrypt.genSalt(10, (err, salt) => {
           if (err) throw err;
-          newUser.password = hash;
-          newUser.save().then(user => res.json(user)).catch(err => console.log(err));
-        })
-      })
-    }
-  }).catch(err => console.log(err));
-})
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
