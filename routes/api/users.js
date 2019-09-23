46f572aaa30aad2912198372b31aa36ec75c60cd
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-09-23 11:26:44 -0700

    added api/users/current

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 // @route   POST api/users/register
 // @desc    Register a user
@@ -40,5 +43,67 @@ router.post("/register", (req, res) => {
     })
     .catch(err => console.log(err));
 });
+
+// @route  POST api/users/login
+// @description Login a user / return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email (email = email)
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({
+          email: "User not found"
+        });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            return res.status(400).json({
+              password: "Password does not match."
+            });
+          }
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          };
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              return res.json({
+                sucess: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
+// @route  GET api/users/current
+// @description return current user
+// @access Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      email: req.user.email,
+      name: req.user.name
+    });
+  }
+);
 
 module.exports = router;
