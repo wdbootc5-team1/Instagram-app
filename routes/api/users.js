62fe9ba34784cb99db99b7bb4889fa508306c402
pranitashakya <53668540+pranitashakya@users.noreply.github.com>
2019-09-23 21:35:54 -0700

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,8 +11,7 @@ const passport = require("passport");
 // @desc    Register a user
 // @access  Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
+  User.findOne({ email: req.body.email }).then(user => {
       if (user) {
         return res.status(400).json({ email: "Email already exists" });
       } else {
