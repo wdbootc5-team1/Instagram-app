12a28492e3d7ce7f9016c780b26af32fc06096d0
msuphap <suphap.ds@gmail.com>
2019-10-07 23:54:55 -0700

    Landing and register

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <table align="center">
+                  <tr>
+                    <td><img src={require('../../img/index1.jpg')}/></td>
+                    <td>
+                      <h1 className="display-3 mb-4">Instagram</h1>
+                      <p className="lead"> Create an Instagram profile and share posts to the world</p>
+                      <hr />
+                      <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+                      <Link to="/login" className="btn btn-lg btn-light">Login</Link>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
