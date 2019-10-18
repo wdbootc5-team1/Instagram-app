d765f8f52dd72cb327d7c2ff6e2e135d17f7b7c5
Msuphap <suphap.ds@gmail.com>
2019-10-17 22:23:44 -0700

    fix proxy and edit typo

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 const initialState = {};
 const middleware = [thunk];
 
-const store = creatStore(
+const store = createStore(
   rootReducer,
   initialState,
   compose(
