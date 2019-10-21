c89fb3d69afabbb137a115ec8a892a6386e23310
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-10-21 14:33:27 -0700

    add Login code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
@@ -8,23 +8,47 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import { Provider } from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
-function App() {
-  return (
-     <Provider store={ store }>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/Login" component={Login} />
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get the user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/Login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
-     </Provider>
-  );
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -17,26 +18,33 @@ class Login extends Component {
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
-    })
+    });
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
-    const newUser = {
+    const currentUser = {
       email: this.state.email,
       password: this.state.password
-    };
+    }
 
-    axios.post('/api/users/login', newUser).then(res => console.log(res.data))
-      .catch(err => this.setState({
-        errors: err.response.data
-      }));
+    this.props.loginUser(currentUser);
 
   }
 
   render() {
     const { errors } = this.state;
-
     return (
       <div className="login">
         <div className="container">
@@ -65,4 +73,15 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
