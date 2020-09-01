50c4e06ef1885049c0f0024202ccad977c124830
sallysy8 <sallysy8@hotmail.com>
2020-09-01 13:51:49 -0700

    ready to deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 const app = express();
 
 //Body parser middleware
@@ -30,6 +31,13 @@ app.get("/", (req, res) => res.send("hello!"));
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = 5700;
 app.listen(port, () => console.log(`Server running on port ${port}`));
